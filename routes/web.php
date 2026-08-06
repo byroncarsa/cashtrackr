46da838ed27e858b61addb1432b2a5394bace617
byroncarsa <byron.carsalade@gmail.com>
2026-08-06 18:49:27 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/register', [RegisterController::class, 'index'])->name('register');
+
+Route::get('/auth/login', [LoginController::class, 'index'])->name('login');
+
+
+
